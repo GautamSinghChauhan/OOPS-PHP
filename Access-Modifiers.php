9124738cb632f25c8d5ecf2e5b0c165a8768db20
GautamSinghChauhan <108332394+GautamSinghChauhan@users.noreply.github.com>
2022-09-10 19:27:57 +0530

    Add files via upload

---
+++ Access-Modifiers.php
@@ -0,0 +1,77 @@
+<?php
+
+class base {
+
+    Protected $name;
+
+    public function __construct($n){
+        $this->name=$n;
+}
+    public function show(){
+       echo "Your Name :".$this->name."<br>";
+
+}
+}
+
+
+$test= new base("Gautam");
+// $test->name="Thakur";
+$test->show();
+
+
+class base1 {
+
+    Protected $name;
+
+    public function __construct($n){
+        $this->name=$n;
+}
+    Protected function show(){
+       echo "Your Name :".$this->name."<br>";
+
+}
+}
+
+class derived  extends base1{
+    public function get(){
+        echo "Your Name :".$this->name."<br>";
+ 
+ }
+}
+
+
+$test= new derived("Gautam Thakur");
+// $test->name="Thakur";
+$test->get();
+
+
+//-----------------Private 
+
+class base2 {
+
+    private $name;
+
+    public function __construct($n){
+        $this->name=$n;
+}
+    public function show(){
+       echo "Your Name :".$this->name."<br>";
+
+}
+}
+
+class derived2  extends base2{
+    public function get(){
+        echo "Your Name :".$this->name."<br>";
+ 
+ }
+}
+
+
+$test= new base2("Gautam Thakur Chauhan");
+// $test->name="Thakur";
+$test->show();
+
+
+ 
+?>
